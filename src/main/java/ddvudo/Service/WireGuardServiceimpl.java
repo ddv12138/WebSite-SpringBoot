de9568381f63f64956ba75208c0618cd088af5d5
ddv12138 <644077730@qq.com>
2020-09-30 17:30:27 +0800

    wg配置前端管理

--- src/main/java/ddvudo/Service/WireGuardServiceimpl.java
+++ src/main/java/ddvudo/Service/WireGuardServiceimpl.java
@@ -1,6 +1,5 @@
 package ddvudo.Service;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import ddvudo.ORM.Mapper.WireGuardConfigMapper;
 import ddvudo.ORM.POJO.Peer;
 import ddvudo.ORM.POJO.WireGuardConfig;
@@ -61,7 +60,7 @@ public class WireGuardServiceimpl implements WireGuardService {
 		Template template = FreeMarkConfig.getTemplate("wgConfigTemplate.ftl");
 		List<String> res = new LinkedList<>();
 		for (int i = 0; i < configList.size(); i++) {
-			OutputStream output = new ByteOutputStream();
+			OutputStream output = new ByteArrayOutputStream();
 			out = new BufferedWriter(new OutputStreamWriter(output));
 			template.process(configList.get(i), out);
 			out.flush();
