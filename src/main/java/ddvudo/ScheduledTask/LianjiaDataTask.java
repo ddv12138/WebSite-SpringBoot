93f16460a988200e0e76e5ea2fb64b5e4997ff01
ddv12138 <644077730@qq.com>
2020-10-20 18:16:29 +0800

    调整链家数据获取间隔

--- src/main/java/ddvudo/ScheduledTask/LianjiaDataTask.java
+++ src/main/java/ddvudo/ScheduledTask/LianjiaDataTask.java
@@ -26,7 +26,8 @@ public class LianjiaDataTask {
 	final RedisTemplate<String, String> redisTemplate;
 	final String[] cities = {"深圳", "武汉"};
 
-	public LianjiaDataTask(CommunityService communityService, CityService cityService, DistrictService districtService, RedisTemplate<String, String> redisTemplate) {
+	public LianjiaDataTask(CommunityService communityService, CityService cityService, DistrictService districtService,
+						   RedisTemplate<String, String> redisTemplate) {
 		this.communityService = communityService;
 		this.cityService = cityService;
 		this.districtService = districtService;
@@ -36,27 +37,37 @@ public class LianjiaDataTask {
 
 	@Scheduled(fixedRate = 1000 * 60 * 60 * 24, initialDelay = 180 * 1000)
 	public void getCommunityDataByCity() {
-		for (String citystr : cities) {
-			City city = cityService.selectByName(citystr);
-			List<District> districtList = districtService.selectByCity(city);
-			for (District district : districtList) {
-				List<Community> communities = communityService.fetchCommunityDataByDistrictFromLianJia(district);
-				for (Community community : communities) {
-					redisTemplate.opsForList().leftPush("community", community.getName() + "__" + LocalDate.now().toString());
-					String url = "https://restapi.amap.com/v3/assistant/coordinate/convert?" +
-							"locations=" + community.getLongitude() + "," + community.getLatitude() + "&" +
-							"coordsys=baidu&" +
-							"output=json&" +
-							"key=5e842e2d890e0361743c15a6e1ec168a";
-					String resStr = Global.doGetHttpRequest(url);
-					redisTemplate.opsForValue().set("httpres", resStr);
-					JSONObject res = JSON.parseObject(resStr);
-					if (res.getIntValue("status") == 1) {
-						community.setGaode_lng(res.getString("locations").split(",")[0]);
-						community.setGaode_lat(res.getString("locations").split(",")[1]);
-						communityService.updateOneLoc(community);
-						Global.Logger(this).info(communities.size() + "/" + communities.indexOf(community));
-					}
+		int index = 0;
+		String citystr = cities[index];
+		if (null != redisTemplate.opsForValue().get("currentCityIndex")) {
+			index = Integer.parseInt(redisTemplate.opsForValue().get("currentCityIndex"));
+			if (index >= cities.length) {
+				index = 0;
+			}
+			citystr = cities[index];
+			index++;
+		}
+		redisTemplate.opsForValue().set("currentCityIndex", index + "");
+		City city = cityService.selectByName(citystr);
+		List<District> districtList = districtService.selectByCity(city);
+		for (District district : districtList) {
+			List<Community> communities = communityService.fetchCommunityDataByDistrictFromLianJia(district);
+			for (Community community : communities) {
+				redisTemplate.opsForList()
+						.leftPush("community", community.getName() + "__" + LocalDate.now().toString());
+				String url = "https://restapi.amap.com/v3/assistant/coordinate/convert?" +
+						"locations=" + community.getLongitude() + "," + community.getLatitude() + "&" +
+						"coordsys=baidu&" +
+						"output=json&" +
+						"key=5e842e2d890e0361743c15a6e1ec168a";
+				String resStr = Global.doGetHttpRequest(url);
+				redisTemplate.opsForValue().set("httpres", resStr);
+				JSONObject res = JSON.parseObject(resStr);
+				if (res.getIntValue("status") == 1) {
+					community.setGaode_lng(res.getString("locations").split(",")[0]);
+					community.setGaode_lat(res.getString("locations").split(",")[1]);
+					communityService.updateOneLoc(community);
+					Global.Logger(this).info(communities.size() + "/" + communities.indexOf(community));
 				}
 			}
 		}
