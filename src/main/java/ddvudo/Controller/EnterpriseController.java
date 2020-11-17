02143f6f8db12790d9a37e1ba54d2535098a52b1
ddv12138 <644077730@qq.com>
2020-11-17 09:03:59 +0800

    研究游标,改善日志

--- src/main/java/ddvudo/Controller/EnterpriseController.java
+++ src/main/java/ddvudo/Controller/EnterpriseController.java
@@ -1,9 +1,7 @@
 package ddvudo.Controller;
 
 import ddvudo.ScheduledTask.ElasticSearchTask;
-import ddvudo.Service.Services.EnterpriseRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +15,8 @@ public class EnterpriseController {
 	@Autowired
 	ElasticSearchTask task;
 
+	public static Thread transThread = null;
+
 	@RequestMapping("/getEnterpriseList")
 	@ResponseBody
 	public HashMap<String, String> listEnterprise(@RequestBody HashMap<String, String> pars) {
@@ -25,7 +25,14 @@ public class EnterpriseController {
 
 	@RequestMapping("/transDataToEs")
 	public boolean transDataToEs() {
-		new Thread(() -> task.doTask()).start();
+		transThread.setName("dataThread");
+		transThread = new Thread(() -> task.doTask());
+		transThread.start();
 		return true;
+	}
+
+	@RequestMapping("/transStates")
+	public Thread.State transStates() {
+		return transThread.getState();
 	}
 }
